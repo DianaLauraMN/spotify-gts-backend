182ce9b8451f6a08cd70d5a2eeeb83022f567e21
Diana Medina <dianaalauramn@gmail.com>
2023-11-16 15:42:19 -0700

    Tracks Repository Refactor

--- src/repositories/TracksRepository.ts
+++ src/repositories/TracksRepository.ts
@@ -6,10 +6,10 @@ import Artist from "../entities/artist/Artist";
 
 class TracksRepository {
 
-    async getTrackById(superToken: string | undefined, trackId: string): Promise<Track | undefined> {
+    async getTrackById(access_token: string | undefined, trackId: string): Promise<Track | undefined> {
         try {
             const trackResponse = await axios.get(`https://api.spotify.com/v1/tracks/${trackId}`, {
-                headers: { 'Authorization': superToken }
+                headers: { 'Authorization': access_token }
             });
             const track: Track = TrackAdapter.adaptTrack(trackResponse.data);
             return track;
@@ -19,10 +19,10 @@ class TracksRepository {
         }
     }
 
-    async getTracksByName(superToken: string | undefined, itemName: string): Promise<Track[] | undefined> {
+    async getTracksByName(access_token: string | undefined, itemName: string): Promise<Track[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const tracksByName = await getTypedItemTracksByType('track', itemName, 50, superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const tracksByName = await getTypedItemTracksByType('track', itemName, 50, access_token);
             return tracksByName;
         } catch (error) {
             console.log("Error while getting Tracks by its name");
@@ -30,10 +30,10 @@ class TracksRepository {
         }
     }
 
-    async getUserTopTracks(superToken: string | undefined): Promise<Track[] | undefined> {
+    async getUserTopTracks(access_token: string | undefined): Promise<Track[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const userTopTracks = await loadTopTracks(superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const userTopTracks = await loadTopTracks(access_token);
             const userTopTracksTyped = getTracksListsTyped(userTopTracks);
             return userTopTracksTyped;
         } catch (error) {
@@ -42,11 +42,11 @@ class TracksRepository {
         }
     }
 
-    async getUserSavedTracks(superToken: string | undefined): Promise<Track[] | undefined> {
+    async getUserSavedTracks(access_token: string | undefined): Promise<Track[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
+            if (!access_token) throw console.error('Error, token expected');
             const savedTracksResponse = await axios.get('https://api.spotify.com/v1/me/tracks', {
-                headers: { 'Authorization': superToken },
+                headers: { 'Authorization': access_token },
                 params: {
                     market: 'ES',
                     limit: 50,
@@ -62,18 +62,18 @@ class TracksRepository {
         }
     }
 
-    async getUserPlaylistsTracks(superToken: string | undefined): Promise<Track[] | undefined> {
+    async getUserPlaylistsTracks(access_token: string | undefined): Promise<Track[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
+            if (!access_token) throw console.error('Error, token expected');
             const playlistsResponse = await axios.get('https://api.spotify.com/v1/me/playlists', {
-                headers: { 'Authorization': superToken },
+                headers: { 'Authorization': access_token },
                 params: {
                     limit: 50,
                     offset: 0,
                 }
             });
             const playlistsId = playlistsResponse.data.items.map((playlist: { id: any }) => playlist.id);
-            const allTracksTyped = await getPlaylistsTracksTyped(playlistsId, superToken);
+            const allTracksTyped = await getPlaylistsTracksTyped(playlistsId, access_token);
             return allTracksTyped;
         } catch (error) {
             console.log('Error while getting current user Playlists (owned and followed)');
@@ -81,13 +81,13 @@ class TracksRepository {
         }
     }
 
-    async getArtistTopTracks(superToken: string | undefined, itemName: string): Promise<Track[] | undefined> {
+    async getArtistTopTracks(access_token: string | undefined, itemName: string): Promise<Track[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const artists = await getTypedArtistsByName(itemName, 50, superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const artists = await getTypedArtistsByName(itemName, 50, access_token);
             const artistRequired = artists.find((artist: { id: string; name: string; }) => artist.name.toLowerCase() === itemName.toLowerCase());
             const artistTopTracksResponse = await axios.get(`https://api.spotify.com/v1/artists/${artistRequired.id}/top-tracks`, {
-                headers: { 'Authorization': superToken },
+                headers: { 'Authorization': access_token },
                 params: {
                     market: 'ES',
                 }
@@ -100,10 +100,10 @@ class TracksRepository {
         }
     }
 
-    async getArtistAllTracks(superToken: string | undefined, artistName: string): Promise<Track[] | undefined> {
+    async getArtistAllTracks(access_token: string | undefined, artistName: string): Promise<Track[] | undefined> {
         try {
             const allArtistTracksResponse = await axios.get(`https://api.spotify.com/v1/search`, {
-                headers: { 'Authorization': superToken },
+                headers: { 'Authorization': access_token },
                 params: {
                     q: `artist:"${artistName}"`,
                     type: 'track',
@@ -119,12 +119,12 @@ class TracksRepository {
         }
     }
 
-    async getUserRecommendations(superToken: string | undefined) {
+    async getUserRecommendations(access_token: string | undefined) {
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const topArtists = await loadTopArtists(10, superToken);
-            const topTracks = await loadTopTracks(superToken);
-            const topGenres = await getUserTopGenresSeeds(topArtists, superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const topArtists = await loadTopArtists(10, access_token);
+            const topTracks = await loadTopTracks(access_token);
+            const topGenres = await getUserTopGenresSeeds(topArtists, access_token);
 
             const topFiveArtist = getSubsetArray(topArtists, 5);
             const topFiveTracks = getSubsetArray(topTracks, 5);
@@ -137,9 +137,9 @@ class TracksRepository {
             const commaSeparatedTracksIds = topFiveTracksId.join(',');
             const commaSeparatedGenresIds = topFiveGenres.join(',');
 
-            const artistRecommendations = await getItemRecommendations('artist', commaSeparatedArtistsIds, 20, superToken);
-            const genresRecommendations = await getItemRecommendations('genres', commaSeparatedGenresIds, 20, superToken);
-            const tracksRecommendationS = await getItemRecommendations('tracks', commaSeparatedTracksIds, 20, superToken);
+            const artistRecommendations = await getItemRecommendations('artist', commaSeparatedArtistsIds, 20, access_token);
+            const genresRecommendations = await getItemRecommendations('genres', commaSeparatedGenresIds, 20, access_token);
+            const tracksRecommendationS = await getItemRecommendations('tracks', commaSeparatedTracksIds, 20, access_token);
 
             const artistRecommendationsTyped = getTracksListsTyped(artistRecommendations.tracks);
             const genresRecommendationsTyped = getTracksListsTyped(genresRecommendations.tracks);
@@ -154,15 +154,15 @@ class TracksRepository {
     }
 
 
-    async getUserTopGenresTracks(superToken: string | undefined): Promise<Track[] | undefined> {
+    async getUserTopGenresTracks(access_token: string | undefined): Promise<Track[] | undefined> {
         let userTopGenresTracks: Track[] = [];
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const userTopArtists = await loadTopArtists(3, superToken);
-            const userTopGenres = await getUserTopGenresSeeds(userTopArtists, superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const userTopArtists = await loadTopArtists(3, access_token);
+            const userTopGenres = await getUserTopGenresSeeds(userTopArtists, access_token);
 
             for (const genre of userTopGenres) {
-                const genreTracksTyped = await this.getTracksByGenre(superToken, genre);
+                const genreTracksTyped = await this.getTracksByGenre(access_token, genre);
                 userTopGenresTracks.push(...genreTracksTyped);
             }
             return userTopGenresTracks;
@@ -172,10 +172,10 @@ class TracksRepository {
         }
     }
 
-    async getTracksByGenre(superToken: string, genreName: string): Promise<Track[] | undefined> {
+    async getTracksByGenre(access_token: string, genreName: string): Promise<Track[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const genreTracks = await getTypedItemTracksByType('playlist', genreName, 1, superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const genreTracks = await getTypedItemTracksByType('playlist', genreName, 1, access_token);
             const genreTracksLimited = genreTracks.slice(0, 15);
             return genreTracksLimited;
         } catch (error) {
@@ -185,12 +185,12 @@ class TracksRepository {
     }
 
 
-    async getUserTopGenres(superToken: string | undefined): Promise<string[] | undefined> {
+    async getUserTopGenres(access_token: string | undefined): Promise<string[] | undefined> {
         try {
-            if (!superToken) throw console.error('Error, token expected');
-            const topArtistsList = await loadTopArtists(10, superToken);
+            if (!access_token) throw console.error('Error, token expected');
+            const topArtistsList = await loadTopArtists(10, access_token);
             const topArtists = getArtistsListTyped(topArtistsList);
-            const sortedTopUserGenres = await getUserTopGenresSeeds(topArtists, superToken);
+            const sortedTopUserGenres = await getUserTopGenresSeeds(topArtists, access_token);
             return sortedTopUserGenres;
         } catch (error) {
             console.log('Error while getting user top genres');
@@ -200,10 +200,10 @@ class TracksRepository {
 
 }
 
-async function getUserTopGenresSeeds(userTopArtists: any[], superToken: string): Promise<string[]> {
+async function getUserTopGenresSeeds(userTopArtists: any[], access_token: string): Promise<string[]> {
     const artistsGenresLists = userTopArtists.map(artist => artist.genres); // top user generos con repeticiones
     const artistsGenresList = artistsGenresLists.flatMap(arr => arr); //combina todos los generos de dif arreglos en un solo arreglo de generos
-    const sortedTopUserGenres = await sortUserTopGenresSeeds(artistsGenresList, superToken);
+    const sortedTopUserGenres = await sortUserTopGenresSeeds(artistsGenresList, access_token);
     const transformedArray = sortedTopUserGenres.map((string) => {
         return formatString(string);
     });
@@ -216,9 +216,9 @@ function formatString(text: string): string {
     return capitalizedString;
 }
 
-async function sortUserTopGenresSeeds(genresList: any[], superToken: string): Promise<string[]> {
+async function sortUserTopGenresSeeds(genresList: any[], access_token: string): Promise<string[]> {
     const genreCounts: Record<string, number> = {};
-    const genresLoaded = await loadAllGenres(superToken);
+    const genresLoaded = await loadAllGenres(access_token);
 
     genresList.forEach((genre) => {
         genreCounts[genre] = (genreCounts[genre] || 0) + 1;
@@ -246,11 +246,11 @@ function validateItemsType(arr: Track[] | Artist[] | undefined, type: string): b
     return isValid;
 
 }
-export async function getTypedItemTracksByType(type: string, itemName: string, limit: number, superToken: string): Promise<Track[] | undefined> {
+export async function getTypedItemTracksByType(type: string, itemName: string, limit: number, access_token: string): Promise<Track[] | undefined> {
     let itemsMapped: any[''];
     try {
         const itemResponse = await axios.get(`https://api.spotify.com/v1/search`, {
-            headers: { 'Authorization': superToken },
+            headers: { 'Authorization': access_token },
             params: {
                 q: itemName,
                 type: type,
@@ -264,7 +264,7 @@ export async function getTypedItemTracksByType(type: string, itemName: string, l
                 itemsMapped = getTracksListsTyped(itemResponse.data.tracks.items);
             } else if (type === 'playlist') {
                 const playlistsId = itemResponse.data.playlists.items.map((playlist: { id: any }) => playlist.id);
-                itemsMapped = await getPlaylistsTracksTyped(playlistsId, superToken);
+                itemsMapped = await getPlaylistsTracksTyped(playlistsId, access_token);
             }
             return itemsMapped;
         }
@@ -282,7 +282,7 @@ function getTracksListsTyped(items: any[]): Track[] {
     return typedTracks;
 }
 
-async function getItemRecommendations(type: string, itemList: string, limit: number, superToken: string) {
+async function getItemRecommendations(type: string, itemList: string, limit: number, access_token: string) {
     let seedArtist = "";
     let seedGenres = "";
     let seedTracks = "";
@@ -295,7 +295,7 @@ async function getItemRecommendations(type: string, itemList: string, limit: num
             seedTracks = itemList;
         }
         const recommendationsResponse = await axios.get('https://api.spotify.com/v1/recommendations', {
-            headers: { 'Authorization': superToken },
+            headers: { 'Authorization': access_token },
             params: {
                 limit: limit,
                 market: 'ES',
@@ -311,10 +311,10 @@ async function getItemRecommendations(type: string, itemList: string, limit: num
     }
 }
 
-async function loadTopTracks(superToken: string) {
+async function loadTopTracks(access_token: string) {
     try {
         const topTracksResponse = await axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0`, {
-            headers: { 'Authorization': superToken }
+            headers: { 'Authorization': access_token }
         });
         return topTracksResponse.data.items;
     } catch (error) {
@@ -323,11 +323,11 @@ async function loadTopTracks(superToken: string) {
     }
 }
 
-async function loadAllGenres(superToken: string) {
+async function loadAllGenres(access_token: string) {
     ///let generesSeeds;
     // try {
     //     const genresSeedsResponse = await axios.get(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {
-    //         headers: { 'Authorization': superToken }
+    //         headers: { 'Authorization': access_token }
     //     });
 
     //     if (genresSeedsResponse.data.genres) {
@@ -340,13 +340,13 @@ async function loadAllGenres(superToken: string) {
     return getGenresSeedsCppySpotify().genres;
 }
 
-async function getPlaylistsTracksTyped(playlistsId: any[], superToken: string): Promise<Track[]> {
+async function getPlaylistsTracksTyped(playlistsId: any[], access_token: string): Promise<Track[]> {
     try {
         const allTracks: any[] = [];
         const promises = playlistsId.map(async (playlistIterador) => {
             const id = playlistIterador;
             const playlistsResponse = await axios.get(`https://api.spotify.com/v1/playlists/${id}`, {
-                headers: { 'Authorization': superToken }, params: {
+                headers: { 'Authorization': access_token }, params: {
                     market: 'ES',
                     fields: 'fields=tracks.items(track(name,href,album(name,href)))',
                 }

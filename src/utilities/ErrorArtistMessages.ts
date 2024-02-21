ce79691d755d766e73602a8495be3a798b8f026d
Diana Medina <dianaalauramn@gmail.com>
2024-02-20 18:31:22 -0700

    Creation of every entity error messages and status code

---
+++ src/utilities/ErrorArtistMessages.ts
@@ -0,0 +1,9 @@
+const ErrorArtistMessages = {
+    ARTIST_NAME: 'Error, artist name expected',
+    TOKEN_EXPECTED: 'Error, access token expected',
+    GETTING_ARTIST_BY_NAME: 'Error, while getting artists by name',
+    GETTING_ARTIST_BY_ID: 'Error, while getting artist by name',
+    GETTING_USER_TOP_ARTISTS: 'Error, while getting user top artists'
+}
+
+export { ErrorArtistMessages };
---
+++ src/utilities/ErrorStatusCode.ts
@@ -0,0 +1,7 @@
+enum ErrorStatusCode {
+    BAD_REQUEST_ERROR = 400,
+    TOKEN_EXPECTED_ERROR = 403,
+    NOT_FOUND_ERROR = 404,
+}
+
+export default ErrorStatusCode;
